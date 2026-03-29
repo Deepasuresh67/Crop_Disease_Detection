5cfeda6a07f191be20ef6ac30499bb6f5cb095ad
Deepasuresh67 <deepakaviya023@gmail.com>
2026-03-29 16:15:24 +0530

    Update allowed hosts for Azure

--- crop_disease/settings.py
+++ crop_disease/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure-=9z^a3+xg)$qhhk1_7112_zc0!ix&eeu3htdw3=3dh04m2f1w$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "*",
+    ".azurewebsites.net",
+]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://crop-disease-app-b3bmgtbaa9edeff4.centralindia-01.azurewebsites.net"
